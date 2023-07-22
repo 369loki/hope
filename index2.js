42ec3f34bf37ec1e849cdca105f8f6aa2c9e7030
loki <131357693+369loki@users.noreply.github.com>
2023-07-22 17:48:00 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,144 @@
+ 
+   var gen = Math.random();
+var gen= gen*101;
+var loki = Math.floor(gen);
+  
+ 
+ document.querySelector(".poda").innerHTML = (loki+"%");
+ 
+ 
+ 
+
+ 
+ 
+ if (loki<=5)
+  {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","01.gif");}
+ 
+ 
+ 
+  else if (loki>=6&& loki<=10)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","02.gif");}
+ 
+ 
+ 
+ 
+ else if (loki>=11&& loki<=15)
+  {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","03.gif");}
+ 
+ 
+ 
+  else if (loki>=16&&loki<=20)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","04.gif");}
+ 
+
+
+else if (loki>=21 && loki<=25)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","05.gif");}
+ 
+  
+ 
+  else if (loki>=26&&loki<=30)
+  {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","06.gif");}
+ 
+
+
+else if (loki>=31 && loki<=35)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","07.gif");}
+ 
+  
+ 
+  else if (loki>=36 &&loki<=40)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","08.gif");}
+ 
+
+
+else if (loki>=41 && loki<=45)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","09.gif");}
+ 
+  
+ 
+  else if (loki>=46&&loki<=50)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","10.gif");}
+ 
+
+
+else if (loki>=51 && loki<=55)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","11.gif");}
+ 
+  
+ 
+  else if (loki>=56&&loki<=60)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","12.gif");}
+ 
+
+
+else if (loki>=61 && loki<=65)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","13.gif");}
+ 
+  
+ 
+  else if (loki>=66&&loki<=70)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","14.gif");}
+ 
+
+
+else if (loki>=71 && loki<=75)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","15.gif");}
+ 
+  
+ 
+  else if (loki>=76&&loki<=80)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","16.gif");}
+ 
+
+
+else if (loki>=81 && loki<=85)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","17.gif");}
+ 
+ 
+else if (loki>=86 && loki<=90)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","18.gif");}
+ 
+ 
+else if (loki>=91 && loki<=95)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","19.gif");}
+ 
+ 
+else if (loki>=95 && loki<=100)
+ {var boto =document.querySelector(".pic");
+ boto.setAttribute("src","20.gif");}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
